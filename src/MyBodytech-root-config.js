08582a9ee2184ddbb68bc51b5cb365908ae0dfbd
Jaider-QBM <jaiderstivenquimbaya9@gmail.com>
2025-05-09 21:39:00 -0500

    configuration of the environment linkage of microfrontends

---
+++ src/MyBodytech-root-config.js
@@ -0,0 +1,27 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout,{
+  loaders: {
+    topNav: "<h1>Cargando el menu</h1>",
+  },
+  errors: {
+    topNav: "<h1>Error al Cargar el menu</h1>",
+  },
+});
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return import(/* webpackIgnore: true */ name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
